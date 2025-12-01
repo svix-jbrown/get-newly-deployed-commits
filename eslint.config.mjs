cffb09d87c8777ffb3c402cc5293a84e83996c30
James Brown <jbrown@svix.com>
2025-12-01 11:50:24 -0800

    eslint drives me nuts, man

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,26 @@
 import js from "@eslint/js";
 import globals from "globals";
-import { defineConfig } from "eslint/config";
+import prettier from "eslint-plugin-prettier";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
+  globalIgnores(["**/coverage", "**/dist", "**/linter", "**/node_modules"]),
   {
     files: ["**/*.{js,mjs,cjs}"],
-    plugins: { js },
+    plugins: { js, prettier },
     extends: ["js/recommended"],
-    languageOptions: { globals: globals.browser },
+    languageOptions: {
+      globals: globals.node,
+      ecmaVersion: 2023,
+      sourceType: "module",
+    },
+    rules: {
+      "eslint-comments/no-use": "off",
+      "eslint-comments/no-unused-disable": "off",
+      "i18n-text/no-en": "off",
+      "import/no-namespace": "off",
+      "no-console": "off",
+      "prettier/prettier": "error",
+    },
   },
 ]);
